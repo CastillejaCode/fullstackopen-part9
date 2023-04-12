6c552b61ad29f5ce14e32c8c77076db18e9c6d1b
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-04-11 18:47:14 -0700

    Finish 9.3

--- node/bmiCalculator.ts
+++ node/bmiCalculator.ts
@@ -1,10 +1,21 @@
 const calculateBMI = (height: number, weight: number): string => {
 	//prettier-ignore
 	const BMI = Math.round((weight / ((height / 100) ** 2)));
-	if (BMI < 18.5) return `Underweight (${BMI}: unhealthy weight)`;
+	if (BMI < 18.5) `Underweight (${BMI}: unhealthy weight)`;
 	if (BMI >= 18.5 && BMI < 25) return `Normal (${BMI}: healthy weight)`;
 	if (BMI >= 25 && BMI < 30) return `Overweight (${BMI}: unhealthy weight)`;
-	else return `Obese (${BMI}: unhealthy weight)`;
+	if (BMI >= 30) return `Obese (${BMI}: unhealthy weight)`;
+	throw new Error('Did you put in correct parameters?');
 };
 
-console.log(calculateBMI(186, 87));
+try {
+	console.log(
+		calculateBMI(parseInt(process.argv[2]), parseInt(process.argv[3]))
+	);
+} catch (error: unknown) {
+	let errorMessage = 'Something went wrong: ';
+	if (error instanceof Error) {
+		errorMessage += error.message;
+	}
+	console.log(errorMessage);
+}
---
+++ node/exerciseCalculator.ts
@@ -0,0 +1,62 @@
+interface Results {
+	periodLength: number;
+	trainingDays: number;
+	target: number;
+	average: number;
+	success: boolean;
+	rating: number;
+	ratingDescription: string;
+}
+
+const calculateExercises = (array: number[], target: number): Results => {
+	const periodLength = array.length;
+	const trainingDays = array.filter((day: number) => day !== 0).length;
+	const average = array.reduce(
+		(prev, current) => prev + current / array.length,
+		0
+	);
+	const rating = (average: number, target: number): number => {
+		const difference = target - average;
+		if (difference <= 0) return 3;
+		if (difference > 0 && difference < 1) return 2;
+		else return 1;
+	};
+
+	const ratingDescription = (rating: number): string => {
+		switch (rating) {
+			case 3:
+				return 'Good job, you reached the target!';
+			case 2:
+				return 'Not bad, keep up the work';
+			case 1:
+				return 'Get it together man, try harder';
+		}
+	};
+
+	return {
+		periodLength,
+		trainingDays,
+		target,
+		average,
+		success: average >= target,
+		rating: rating(average, target),
+		ratingDescription: ratingDescription(rating(average, target)),
+	};
+};
+const [target, array] = [
+	Number(process.argv[2]),
+	process.argv.slice(3).map((day) => Number(day)),
+];
+
+try {
+	if (array.includes(NaN) || typeof target !== 'number') {
+		throw new Error('You should have put in all numbers!');
+	}
+	console.log(calculateExercises(array, target));
+} catch (error: unknown) {
+	let errorMessage = 'Something went wrong: ';
+	if (error instanceof Error) {
+		errorMessage += error.message;
+	}
+	console.log(errorMessage);
+}
