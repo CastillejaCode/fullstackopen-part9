98312ee6aebc6c17a66fd2e2608252ab729c708c
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-04-16 20:59:57 -0700

    Finish 9.19

--- flight-diary-frontend/src/App.tsx
+++ flight-diary-frontend/src/App.tsx
@@ -1,13 +1,17 @@
+import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { getAll, createDiary } from './services/noteService';
 import { Diary } from './types';
 
 const App = () => {
 	const [diaries, setDiaries] = useState<Diary[]>([]);
+
 	const [date, setDate] = useState<string>('');
 	const [weather, setWeather] = useState<string>('');
 	const [visibility, setVisibility] = useState<string>('');
 	const [comment, setComment] = useState<string>('');
+
+	const [notification, setNotification] = useState<string>('');
 
 	useEffect(() => {
 		getAll().then((data) => setDiaries(diaries.concat(data)));
@@ -15,23 +19,31 @@ const App = () => {
 
 	const submitDiary = async (event: React.SyntheticEvent) => {
 		event.preventDefault();
-		const newDiary = {
-			date,
-			weather,
-			visibility,
-			comment,
-			id: diaries.length + 1,
-		};
-		const response = await createDiary(newDiary);
-		await setDiaries(diaries.concat(response));
-		setDate('');
-		setWeather('');
-		setVisibility('');
-		setComment('');
+		try {
+			const newDiary = {
+				date,
+				weather,
+				visibility,
+				comment,
+				id: diaries.length + 1,
+			};
+			const response = await createDiary(newDiary);
+			await setDiaries(diaries.concat(response));
+			setDate('');
+			setWeather('');
+			setVisibility('');
+			setComment('');
+		} catch (error: unknown) {
+			if (axios.isAxiosError(error)) {
+				setNotification(error.response?.data);
+				setInterval(() => setNotification(''), 5000);
+			}
+		}
 	};
 
 	return (
 		<div>
+			{notification && <h2 style={{ color: 'red' }}>{notification}</h2>}
 			<div>
 				<h1>Add new entry</h1>
 				<form action='' onSubmit={submitDiary}>
@@ -39,7 +51,7 @@ const App = () => {
 						<label htmlFor=''>
 							Date
 							<input
-								type='text'
+								type='date'
 								name='date'
 								value={date}
 								onChange={(event) => setDate(event?.target.value)}
@@ -47,23 +59,88 @@ const App = () => {
 						</label>
 					</div>
 					<div>
+						Weather{' '}
 						<label htmlFor=''>
-							Weather
+							sunny
 							<input
-								type='text'
+								type='radio'
 								name='weather'
-								value={weather}
+								value='sunny'
+								onChange={(event) => setWeather(event.target.value)}
+							/>
+						</label>
+						<label htmlFor=''>
+							rainy
+							<input
+								type='radio'
+								name='weather'
+								value='rainy'
+								onChange={(event) => setWeather(event.target.value)}
+							/>
+						</label>
+						<label htmlFor=''>
+							cloudy
+							<input
+								type='radio'
+								name='weather'
+								value='cloudy'
+								onChange={(event) => setWeather(event.target.value)}
+							/>
+						</label>
+						<label htmlFor=''>
+							stormy
+							<input
+								type='radio'
+								name='weather'
+								value='stormy'
+								onChange={(event) => setWeather(event.target.value)}
+							/>
+						</label>
+						<label htmlFor=''>
+							windy
+							<input
+								type='radio'
+								name='weather'
+								value='windy'
 								onChange={(event) => setWeather(event.target.value)}
 							/>
 						</label>
 					</div>
 					<div>
+						Visibility{' '}
 						<label htmlFor=''>
-							Visibility
+							great
 							<input
-								type='text'
+								type='radio'
 								name='vis'
-								value={visibility}
+								value='great'
+								onChange={(event) => setVisibility(event.target.value)}
+							/>
+						</label>
+						<label htmlFor=''>
+							good
+							<input
+								type='radio'
+								name='vis'
+								value='good'
+								onChange={(event) => setVisibility(event.target.value)}
+							/>
+						</label>
+						<label htmlFor=''>
+							ok
+							<input
+								type='radio'
+								name='vis'
+								value='ok'
+								onChange={(event) => setVisibility(event.target.value)}
+							/>
+						</label>
+						<label htmlFor=''>
+							poor
+							<input
+								type='radio'
+								name='vis'
+								value='poor'
 								onChange={(event) => setVisibility(event.target.value)}
 							/>
 						</label>
@@ -82,6 +159,7 @@ const App = () => {
 					<button>Submit</button>
 				</form>
 			</div>
+			<h3>Diary Entries</h3>
 			<ul>
 				{diaries.map((diary) => {
 					return (
